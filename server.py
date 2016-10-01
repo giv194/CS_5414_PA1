e27beeafdaa1f630dd9fe8de6452ee14e2b3cde9
Paul Ammann <meanmrclean@gmail.com>
2016-10-01 14:16:11 -0400

    election is no longer working right now

--- server.py
+++ server.py
@@ -17,18 +17,25 @@ thread_list = []
 
 # Timeout value
 TIMEOUT = 0.3
+HB_TIMEOUT = 0.9*TIMEOUT
+
+GPORT = 20000
 
 # PROCCESS MESSAGE TYPES:
+I_AM_COORDINATOR = "I_AM_COORDINATOR"
 ELECTION_ACK = "ELECTION_ACK"
 UR_ELECTED = "UR_ELECTED"
+HEARTBEAT = "HEARTBEAT"
+HEARTBEAT_ACK = "HEARTBEAT_ACK"
 
 class TimeOut():
-    def __init__(self):
-        self.end = time.time()+TIMEOUT
+    def __init__(self, time_out_period = TIMEOUT):
+        self.time_out_period = time_out_period
+        self.end = time.time()+time_out_period
     def waiting(self):
-        return time.time() > self.end
+        return time.time() < self.end
     def reset(self):
-        self.end = time.time()+TIMEOUT
+        self.end = time.time() + self.time_out_period
 
 class Server(threading.Thread):
     def __init__(self, port, process):
@@ -57,25 +64,37 @@ class Server(threading.Thread):
         self.open_socket()
         input = [self.server,sys.stdin]
         running = 1
-        self.process.t_o.reset();
+        t_o = TimeOut()
+        hb_t_0 = TimeOut(HB_TIMEOUT)
         while running:
-            if self.port >= 20000 and self.port <= 29999 and not self.process.isCoordinator() and (not self.process.coordinator or not self.process.t_o.waiting()):
-                self.process.elect_coordinator()
+            if self.port >= GPORT and self.port <= 29999:
+                if not self.process.isCoordinator():
+                    if not t_o.waiting():
+                        print "timeout"
+                        self.process.elect_coordinator()
+                else:
+                    if not hb_t_0.waiting():
+                        for p_id in range(0,process.n):
+                            Connection_Client(GPORT+p_id, p_id, HEARTBEAT)
+                        hb_t_0.reset()
             inputready,outputready,exceptready = select.select(input,[],[])
             for s in inputready:
 
                 if s == self.server:
                     # handle the server socket
+                    if (self.port - GPORT) == process.coordinator:
+                        print "reset?"
+                        t_o.reset()
                     c = Client(self.server.accept(), self.process, self.port)
                     c.start()
                     self.threads.append(c)
 
-                elif s == sys.stdin:
-                    # handle standard input
-                    junk = sys.stdin.readline()
-                    print "you printed "+junk +" for "+str(20000+(self.port-1)%2)
-                    # Connection_Client(20000+(self.port-1)%2, self.process.id, junk).run()
-                    running = 0
+                # elif s == sys.stdin:
+                #     # handle standard input
+                #     junk = sys.stdin.readline()
+                #     print "you printed "+junk +" for "+str(GPORT+(self.port-1)%2)
+                #     # Connection_Client(GPORT+(self.port-1)%2, self.process.id, junk).run()
+                #     running = 0
 
         # close all threads
         self.server.close()
@@ -104,9 +123,6 @@ class Client(threading.Thread):
             try:
                 data = self.client.recv(self.size)
                 if data:
-                    # check coordinator heartbeat
-                    if (self.port - 20000) == process.coordinator:
-                        self.process.t_o.reset()
                     self.client.send(data)
                     print data, ':', self.port
                     process_lock.acquire()
@@ -145,7 +161,6 @@ class Process():
         self.n = n
         self.port = self_port
         self.m_port = master_port
-        self.t_o = TimeOut()
         self.songs = {}
         self.state = None
         self.up_set = set([x for x in range(0,n)])
@@ -172,22 +187,28 @@ class Process():
         subprocess.Popen(['./kill_script', str(self.m_port)], stdout=open('/dev/null'), stderr=open('/dev/null'))
 
     def elect_coordinator(self):
-        print "ELECTING NEW COORDINATOR"
         self.up_set.discard(self.coordinator)
-        n_c_pid = min(self.up_set)
-        if n_c_pid == self.id:
+        self.coordinator = min(self.up_set)
+        print "ELECTING NEW COORDINATOR: "+str(self.coordinator)
+        if self.coordinator == self.id:
             print "I AM THE COORDINATOR"
             self.coordinator = self.id
+            for p_id in self.up_set:
+                if p_id != self.id:
+                    Connection_Client(GPORT+ p_id, p_id, I_AM_COORDINATOR)
         else:
             # ping coordinator
-            Connection_Client(20000+n_c_pid, n_c_pid, UR_ELECTED).run()
+            try:
+                Connection_Client(GPORT+n_c_pid, n_c_pid, UR_ELECTED).run()
+            except:
+                self.elect_coordinator()
 
 if __name__ == "__main__":
     p_id = int(sys.argv[1])
     p_n = int(sys.argv[2])
     p_master_port = int(sys.argv[3])
-    process = Process(p_id, p_n, 20000 + p_id, p_master_port)
-    servers = [Server(p_master_port, process), Server(20000 + p_id, process)]
+    process = Process(p_id, p_n, GPORT + p_id, p_master_port)
+    servers = [Server(p_master_port, process), Server(GPORT + p_id, process)]
     for s in servers:
         s.start()
         thread_list.append(s)
